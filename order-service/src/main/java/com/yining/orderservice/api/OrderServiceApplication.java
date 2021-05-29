6ed38a6fc8812e193b0a23947cdfae6ca08277c6
YiningChen001586646 <chen.yinin@northeastern.edu>
2021-05-29 17:41:12 -0400

    Update OrderServiceApplication.java

--- order-service/src/main/java/com/yining/orderservice/api/OrderServiceApplication.java
+++ order-service/src/main/java/com/yining/orderservice/api/OrderServiceApplication.java
@@ -3,6 +3,7 @@ package com.yining.orderservice.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -17,6 +18,7 @@ public class OrderServiceApplication {
         SpringApplication.run(OrderServiceApplication.class, args);
     }
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
 
