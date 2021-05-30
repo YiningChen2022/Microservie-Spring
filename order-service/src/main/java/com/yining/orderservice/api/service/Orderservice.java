6de5d8c5a788288c6b48e627caaf4115b9373c52
YiningChen001586646 <chen.yinin@northeastern.edu>
2021-05-29 22:15:36 -0400

    updates

--- order-service/src/main/java/com/yining/orderservice/api/service/Orderservice.java
+++ order-service/src/main/java/com/yining/orderservice/api/service/Orderservice.java
@@ -6,16 +6,23 @@ import com.yining.orderservice.api.common.TransactionResponse;
 import com.yining.orderservice.api.entity.Order;
 import com.yining.orderservice.api.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@RefreshScope
 public class Orderservice {
     @Autowired
     private OrderRepository repository;
     @Autowired
-    private RestTemplate template;
+    @Lazy
 
+    private RestTemplate template;
+    @Value("${microservice.payment-service.endpoints.endpoint.uri}")
+    private String ENDPOINT_URL;
     public TransactionResponse saveOrder(TransactionRequest request){
         String response="";
         Order order=request.getOrder();
@@ -23,7 +30,7 @@ public class Orderservice {
         payment.setOrderId(order.getId());
         payment.setAmount(order.getPrice());
         //rest call:
-        Payment paymentResponse=template.postForObject("http://PAYMENT-SERVICE/payment/doPayment",payment,Payment.class);
+        Payment paymentResponse=template.postForObject(ENDPOINT_URL,payment,Payment.class);
 
         response=paymentResponse.getPaymentStatus().equals("success")?"payment processing successful and order placed":"there is a failure in payment api, order added to cart";
          repository.save(order);
