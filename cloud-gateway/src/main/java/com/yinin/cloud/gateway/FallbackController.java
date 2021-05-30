2c9bf19b1db61a2d01e5e245423f46d096a36791
YiningChen001586646 <chen.yinin@northeastern.edu>
2021-05-29 20:55:57 -0400

    add fallback

---
+++ cloud-gateway/src/main/java/com/yinin/cloud/gateway/FallbackController.java
@@ -0,0 +1,18 @@
+package com.yinin.cloud.gateway;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class FallbackController {
+    @RequestMapping("/orderFallBack")
+    public Mono<String> orderServiceFallBack(){
+        return Mono.just("Order Service is taking too long to respond or is down,please try again later");
+    }
+    @RequestMapping("/paymentFallBack")
+    public Mono<String> paymentServiceFallBack(){
+        return Mono.just("payment Service is taking too long to respond or is down, please try again later");
+    }
+
+}
